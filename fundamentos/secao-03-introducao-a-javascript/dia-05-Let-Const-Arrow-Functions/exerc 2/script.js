7e89e970a2cf333918f301fc258cfcc61aed7d96
Luiz Lantmann <luizzlantmann@gmail.com>
2023-02-01 20:37:29 -0300

    exercises

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-Let-Const-Arrow-Functions/exerc 2/script.js
@@ -0,0 +1,10 @@
+// 🚀 Transforme a função numeroAleatorio em uma arrow function;
+const numeroAleatorio = () => Math.random();
+console.log(numeroAleatorio());
+
+// 🚀 Transforme a função nomeCompleto em uma arrow function;
+
+const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`;
+let nome = "Ivan";
+let sobrenome = "Pires";
+console.log(nomeCompleto(nome, sobrenome));
