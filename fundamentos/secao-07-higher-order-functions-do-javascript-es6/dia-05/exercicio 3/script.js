da7bd643db9e79f2c2fb05bff1a9dd0b34a8ef1d
Luiz Lantmann <luizzlantmann@gmail.com>
2023-03-16 14:54:16 -0300

    fundamentos finalizado

---
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05/exercicio 3/script.js
@@ -0,0 +1,14 @@
+// const getPosition = (latitude, longitude) => ({
+//   latitude: latitude,
+//   longitude: longitude,
+// });
+
+// console.log(getPosition(-19.8157, -43.9542));
+
+//property shorthand
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+console.log(getPosition(-19.8157, -43.9542));
